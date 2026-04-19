73ed7306a9f016d46551602e84a591c4cef66846
Zella <thanhtai0322@gmail.com>
2026-04-19 14:45:28 +0700

    Tạo vật thể trong Frame
    drawRect: chỉ vẽ
    fillRect: vẽ xong fill màu vào ô

--- src/map/Game.java
+++ src/map/Game.java
@@ -3,11 +3,15 @@ package map;
 import javax.swing.JFrame;
 
 public class Game extends JFrame{
+    private GameScreen gameScreen;
     public Game(){
         setSize(640,640);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        
+        gameScreen = new GameScreen();
+        add(gameScreen);
     }
 
     public static void main (String[] args){
