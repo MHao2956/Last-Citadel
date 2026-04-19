ca48419738b7282df873e46615a49f2fc2dbd71f
ThangMavas <135797303+ThangMavas227@users.noreply.github.com>
2026-04-19 17:30:44 +0700

    setting: a interface for users

---
+++ src/core/Game.java
@@ -0,0 +1,13 @@
+package core;
+
+import javax.swing.*;
+
+public class Game extends JFrame {
+    public Game(){
+        setSize(400,400);
+        setVisible(true);
+    }
+    public static void main (String[] args){
+        Game g = new Game();
+    }
+}
