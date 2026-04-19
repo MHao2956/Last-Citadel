a9ecec3577bb616259dd1a5bf3f1669a0c1e64ff
Zella <thanhtai0322@gmail.com>
2026-04-19 17:42:36 +0700

    Vẽ map 1

--- src/map/GameScreen.java
+++ src/map/GameScreen.java
@@ -3,19 +3,31 @@ package map;
 import java.awt.Color;
 import javax.swing.JPanel;
 import java.awt.Graphics;
+import java.util.Random;
 
 public class GameScreen extends JPanel {
+    private Random random;
     public GameScreen(){
-
+        random = new Random();
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
-        g.setColor(Color.RED);
-        g.fillRect(50, 50, 100, 100);
-        g.fillRect(150, 50, 100, 100);
-        g.fillRect(100, 150, 100, 200);
-        
+        for(int y = 0; y < 20; y++){
+            for(int x = 0; x < 20; x++){
+                g.setColor(getRndColor());
+                g.fillRect(x*32, y*32, 32, 32);
+            }
+        }
+
+    }
+
+    private Color getRndColor(){
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+
+        return new Color(r, g, b);
     }
 }
