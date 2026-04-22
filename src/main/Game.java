f91b033fc11eec9becc52ef18c4ff446f78a3941
ThangMavas <135797303+ThangMavas227@users.noreply.github.com>
2026-04-22 12:36:54 +0700

    add: gameLoop into Game class

--- src/main/Game.java
+++ src/main/Game.java
@@ -10,7 +10,19 @@ public class Game extends JFrame {
 
     private final GameScreen GS;
     private BufferedImage img;
+    private double timePerFrame;
+    private long lastFrame;
+
+    private long lastUpdate;
+    private double timePerUpdate;
+
+    private int updates;
+    private long lastTimeUPS;
+
+
     public Game(){
+        timePerFrame = 1000000000.0 / 120.0;
+        timePerUpdate = 1000000000.0 / 60.0;
         importImg();
         setSize(640,640);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -31,7 +43,39 @@ public class Game extends JFrame {
         }
     }
 
+    private void loopGame(){
+        while(true){
+            if(System.nanoTime() - lastUpdate >= timePerUpdate){
+                updateGame();
+
+                callUPS();
+            }
+            if(System.nanoTime() - lastFrame >= timePerFrame){
+                lastFrame = System.nanoTime();
+                repaint();
+            }else{
+                // do nothing
+            }
+
+        }
+    }
+
+    private void callUPS() {
+        if (System.currentTimeMillis() - lastTimeUPS >= 1000){
+            System.out.println("UPS: " + updates);
+            updates = 0;
+            lastTimeUPS = System.currentTimeMillis();
+        }
+    }
+
+    private void updateGame() {
+        updates++;
+        lastUpdate = System.nanoTime();
+        //System.out.println("Game Updated!");
+    }
+
     public static void main (String[] args){
-        Game g = new Game();
+        Game game = new Game();
+        game.loopGame();
     }
 }
