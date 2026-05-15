bd27465bf93685d2626b3567bd4963cae5b10268
ThangMavas <135797303+ThangMavas227@users.noreply.github.com>
2026-05-15 23:07:12 +0700

    create: class for GameWin

--- src/main/GameStates.java
+++ src/main/GameStates.java
@@ -1,7 +1,7 @@
 package main;
 
 public enum GameStates {
-    PLAYING, MENU, SETTINGS, EDIT, GAME_OVER;
+    PLAYING, MENU, SETTINGS, EDIT, GAME_OVER, GAME_WIN;
 
     public static GameStates gameState = MENU;
 
